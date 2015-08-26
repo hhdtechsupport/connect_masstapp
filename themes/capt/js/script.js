e7760363ee2f7ae392f725f2093db2950d47acbb
Biewener, Matt <mbiewener@edc.org>
2015-08-26 15:58:53 -0400

    a few changes to some mobile issues

--- themes/capt/js/script.js
+++ themes/capt/js/script.js
@@ -1,4 +1,4 @@
-sd/**
+/**
  * @file
  * A JavaScript file for the theme.
  *
@@ -215,7 +215,7 @@ function isPhone () {
       }
 
   /*
-   *  End responsive show-hide functinality
+   *  End responsive show-hide functionality
    */
 
   function showHideIcons (header, body) {
