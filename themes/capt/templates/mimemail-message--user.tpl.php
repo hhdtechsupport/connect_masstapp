f534c7dd3b57b5ad77a13881849092fdbb231a89
NancyDru <nan_wich@bellsouth.net>
2015-10-22 13:47:52 -0400

    #11717829 - Mail problems.

--- themes/capt/templates/mimemail-message--user.tpl.php
+++ themes/capt/templates/mimemail-message--user.tpl.php
@@ -19,8 +19,10 @@
  * @see template_preprocess_mimemail_message()
  */
 ?>
+<!-- mimemail-message__user.tpl.php -->
 <html>
   <head>
+    <title></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <?php if ($css): ?>
     <style type="text/css">
@@ -36,7 +38,8 @@
 
         <table cellpadding="0" cellspacing="0" border="0" width="100%" class="[ics-class:value]">
           <tr>
-            <td background="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png" valign="top" bgcolor="#E3E3DF">
+            <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png"'
+              valign="top" bgcolor="#E3E3DF">
             <!--[if gte mso 9]>
             <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
               <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png" color="#E3E3DF" />
@@ -50,11 +53,8 @@
 
                       <table class="logos center">
                         <tr>
-                          <td width="351" height="78" valign="bottom">
-                            <img src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/capt_logo.png" width="350" height="80" alt="CAPT - SAMHSA's Center for the Application of Prevention Technologies">
-                          </td>
-                          <td width="250" height="78" valign="bottom">
-                            <img src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/samhsa_logo.png" width="250" height="80" alt="SAMHSA - U.S. Substance Abuse and Mental Health Services Administration">
+                          <td width="580">
+                            <div style="font-size: 24px; margin-top: 20px; color: black;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div>
                           </td>
                         </tr>
                       </table>
@@ -67,7 +67,8 @@
 
                       <table class="colorband">
                         <tr>
-                          <td background="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
+                          <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"'
+                            bgcolor="#E4E3E1" width="580" height="6" valign="top">
                           <!--[if gte mso 9]>
                           <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:580px;height:6px;">
                             <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" color="#E4E3E1" />
@@ -88,30 +89,6 @@
 
                 </table>
 
-                <table class="banner center">
-                  <tr>
-                    <td background="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/banner.png" bgcolor="#567583" width="600" height="134" valign="top">
-                    <!--[if gte mso 9]>
-                    <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:600px;height:134px;">
-                      <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/banner.png" color="#567583" />
-                      <v:textbox inset="0,0,0,0">
-                    <![endif]-->
-                      <div>
-                        <div style="font-size: 36px; margin-top: 32px;">
-                          <strong>CAPT</strong> <em style="font-weight: normal;">Connect</em>
-                        </div>
-                        <div style="font-size: 15px; text-transform: uppercase;">
-                          Your access point for all CAPT virtual events
-                        </div>
-                      </div>
-                    <!--[if gte mso 9]>
-                      </v:textbox>
-                    </v:rect>
-                    <![endif]-->
-                    </td>
-                  </tr>
-                </table>
-
                 <table class="center">
                   <tr>
                     <td bgcolor="#FFFFFF">
@@ -130,7 +107,8 @@
 
                       <table class="colorband">
                         <tr>
-                          <td background="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
+                          <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"'
+                            bgcolor="#E4E3E1" width="580" height="6" valign="top">
                           <!--[if gte mso 9]>
                           <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:580px;height:6px;">
                           <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" color="#E4E3E1" />
@@ -154,10 +132,10 @@
 
                       <table class="mailing center">
                         <tr>
-                          <td>
-                            <p>Mailing Address: SAMHSA's Center for the Application of Prevention Technologies<br>Education Development Center, Inc. | 43 Foundry Ave | Waltham, MA 02453-8313</p>
-                            <p><a href="http://dev.captconnect.edc.org">Terms</a> | <a href="http://dev.captconnect.edc.org">Privacy</a> | <a href="http://dev.captconnect.edc.org">Unsubscribe</a></p>
-                            <p id="date">[site:current-date]</p>
+                          <td width="580">
+                            <div style="text-align:left">
+                              <p>Contacts for Questions/Issues: Technical Problems (<a href="mbiewener@edc.org">Matt Biewener</a>), Content and Materials Approval (<a href="madler@edc.org">Melanie Adler</a>)</p>
+                            </div>
                           </td>
                         </tr>
                       </table>
