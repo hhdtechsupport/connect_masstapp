cfd941a3a50b49bc0777e2077424627b4078116b
Biewener, Matt <mbiewener@edc.org>
2015-11-02 12:41:25 -0500

    templates for emails fixes

--- themes/capt/templates/mimemail-message--user.tpl.php
+++ themes/capt/templates/mimemail-message--user.tpl.php
@@ -1,52 +1,152 @@
-<!-- mimemail-message__user.tpl.php -->
+<?php
+
+/**
+ * @file
+ * Default theme implementation to format an HTML mail.
+ *
+ * Copy this file in your default theme folder to create a custom themed mail.
+ * Rename it to mimemail-message--[module]--[key].tpl.php to override it for a
+ * specific mail.
+ *
+ * Available variables:
+ * - $recipient: The recipient of the message
+ * - $subject: The message subject
+ * - $body: The message body
+ * - $css: Internal style sheets
+ * - $module: The sending module
+ * - $key: The message identifier
+ *
+ * @see template_preprocess_mimemail_message()
+ */
+?>
+
+<?php $base_url = $GLOBALS['base_url']; ?>
 
 <html>
   <head>
-    <title></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <?php if ($css): ?>
     <style type="text/css">
-      <?php print $css; ?>
-
-      .main {
-        width: 580px;
-      }
-
-      .footer {
-        background-color: #e4e3e1;
-        width: 580px;
-      }
-
-      .footer p {
-        margin-top: 0;
-        padding: 0 12px 1em 12px;
-        width: 556px;
-      }
-
-      .message-body p,
-      .message-body {
-        padding: 15px;
-        width: 550px;
-      }
+      <!--
+      <?php print $css ?>
+      -->
     </style>
+    <?php endif; ?>
   </head>
-  <body id="mimemail-body">
+  <body id="mimemail-body" <?php if ($module && $key): print 'class="'. $module .'-'. $key .'"'; endif; ?>>
     <div id="center">
       <div id="main">
-        <img src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/banner.png"
-          alt="background" style="width: 580px;"/>
-        <br />
-        <img src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"
-          style="width: 580px; margin-top: 0;">
 
-        <div class="message-body">
-          <?php print $body ?>
-        </div>
+        <table cellpadding="0" cellspacing="0" border="0" width="100%" class="[ics-class:value]">
+          <tr>
+            <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png" valign="top" bgcolor="#E3E3DF">
+              <div>
 
-        <img src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"
-          style="width: 580px;">
-        <div class="footer">
-          <p>Contacts for Questions/Issues: Technical Problems (<a href="mailto:mbiewener@edc.org">Matt Biewener</a>), Content and Materials Approval (<a href="mailto:madler@edc.org">Melanie Adler</a>)</p>
-        </div>
+                <table class="header">
+                  <tr>
+                    <td>
+
+                      <table class="logos center">
+                        <tr>
+                          <td width="351" height="78" valign="bottom">
+                            <img src="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/capt_logo.png" width="350" height="80" alt="CAPT - SAMHSA's Center for the Application of Prevention Technologies" title="CAPT - SAMHSA's Center for the Application of Prevention Technologies">
+                          </td>
+                          <td width="250" height="78" valign="bottom">
+                            <img src="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/samhsa_logo.png" width="250" height="80" alt="SAMHSA - U.S. Substance Abuse and Mental Health Services Administration" title="SAMHSA - U.S. Substance Abuse and Mental Health Services Administration">
+                          </td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+
+                      <table class="colorband">
+                        <tr>
+                          <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
+                            <div>
+                            </div>
+                          </td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+
+                </table>
+
+                <table class="banner center">
+                  <tr>
+                    <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/banner.png" bgcolor="#567583" width="600" height="134" valign="top">
+                      <div>
+
+                        <?php
+                          if (!empty($nid_event->field_banner_text)) {
+                            print '<div style="font-size: 24px; margin-top: 36px; line-height: 1;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 36px; opacity: 0.5;">' .  $nid_event->field_banner_text['und'][0]['value'] . '</div>';
+                          }
+                          else if (!empty($nid_event->field_category)) {
+                            $term = taxonomy_term_load($nid_event->field_category['und'][0]['target_id']);
+                            print '<div style="font-size: 24px; margin-top: 36px; line-height: 1;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 36px; opacity: 0.5;">' .  $term->name . '</div>';
+                          }
+                          else {
+                            print '<div style="font-size: 36px; margin-top: 32px;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 15px; text-transform: uppercase;">Your access point for all CAPT virtual events</div>';
+                          }
+                        ?>
+
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+
+                <table class="center">
+                  <tr>
+                    <td bgcolor="#FFFFFF">
+                      <div style="padding: 15px"><?php print $body; ?></div>
+                    </td>
+                  </tr>
+                </table>
+
+                <table class="footer">
+                  <tr>
+                    <td>
+
+                      <table class="colorband">
+                        <tr>
+                          <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
+                            <div>
+                            </div>
+                          </td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>
+
+                      <table class="mailing center">
+                        <tr>
+                          <td>
+                            <p>Mailing Address: SAMHSA's Center for the Application of Prevention Technologies<br>Education Development Center, Inc. | 43 Foundry Ave | Waltham, MA 02453-8313</p>
+                            <p><a href="<?php print $base_url; ?>">Terms</a> | <a href="<?php print $base_url; ?>">Privacy</a> | <a href="<?php print $base_url; ?>">Unsubscribe</a></p>
+                          </td>
+                        </tr>
+                      </table>
+
+                    </td>
+                  </tr>
+                </table>
+
+              </div>
+            </td>
+          </tr>
+        </table>
+
+
+
       </div>
     </div>
   </body>
--- themes/capt/templates/mimemail-message--workflow-notify.tpl.php
+++ themes/capt/templates/mimemail-message--workflow-notify.tpl.php
@@ -19,10 +19,11 @@
  * @see template_preprocess_mimemail_message()
  */
 ?>
-/* mimemail-message__workflow-notify.tpl.php */
+
+<?php $base_url = $GLOBALS['base_url']; ?>
+
 <html>
   <head>
-    <title></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <?php if ($css): ?>
     <style type="text/css">
@@ -38,13 +39,7 @@
 
         <table cellpadding="0" cellspacing="0" border="0" width="100%" class="[ics-class:value]">
           <tr>
-            <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png"'
-              valign="top" bgcolor="#E3E3DF">
-            <!--[if gte mso 9]>
-            <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="mso-width-percent:1000;">
-              <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png" color="#E3E3DF" />
-              <v:textbox inset="0,0,0,0">
-            <![endif]-->
+            <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/pattern.png" valign="top" bgcolor="#E3E3DF">
               <div>
 
                 <table class="header">
@@ -53,8 +48,11 @@
 
                       <table class="logos center">
                         <tr>
-                          <td width="580">
-                            <div style="font-size: 24px; margin-top: 20px; color: black;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div>
+                          <td width="351" height="78" valign="bottom">
+                            <img src="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/capt_logo.png" width="350" height="80" alt="CAPT - SAMHSA's Center for the Application of Prevention Technologies" title="CAPT - SAMHSA's Center for the Application of Prevention Technologies">
+                          </td>
+                          <td width="250" height="78" valign="bottom">
+                            <img src="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/samhsa_logo.png" width="250" height="80" alt="SAMHSA - U.S. Substance Abuse and Mental Health Services Administration" title="SAMHSA - U.S. Substance Abuse and Mental Health Services Administration">
                           </td>
                         </tr>
                       </table>
@@ -67,19 +65,9 @@
 
                       <table class="colorband">
                         <tr>
-                          <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"' 
-                            bgcolor="#E4E3E1" width="580" height="6" valign="top">
-                          <!--[if gte mso 9]>
-                          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:580px;height:6px;">
-                            <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" color="#E4E3E1" />
-                            <v:textbox inset="0,0,0,0">
-                          <![endif]-->
+                          <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
                             <div>
                             </div>
-                          <!--[if gte mso 9]>
-                            </v:textbox>
-                          </v:rect>
-                          <![endif]-->
                           </td>
                         </tr>
                       </table>
@@ -89,14 +77,33 @@
 
                 </table>
 
+                <table class="banner center">
+                  <tr>
+                    <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/banner.png" bgcolor="#567583" width="600" height="134" valign="top">
+                      <div>
+
+                        <?php
+                          if (!empty($nid_event->field_banner_text)) {
+                            print '<div style="font-size: 24px; margin-top: 36px; line-height: 1;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 36px; opacity: 0.5;">' .  $nid_event->field_banner_text['und'][0]['value'] . '</div>';
+                          }
+                          else if (!empty($nid_event->field_category)) {
+                            $term = taxonomy_term_load($nid_event->field_category['und'][0]['target_id']);
+                            print '<div style="font-size: 24px; margin-top: 36px; line-height: 1;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 36px; opacity: 0.5;">' .  $term->name . '</div>';
+                          }
+                          else {
+                            print '<div style="font-size: 36px; margin-top: 32px;"><strong>CAPT</strong> <em style="font-weight: normal;">Connect</em></div><div style="font-size: 15px; text-transform: uppercase;">Your access point for all CAPT virtual events</div>';
+                          }
+                        ?>
+
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+
                 <table class="center">
                   <tr>
                     <td bgcolor="#FFFFFF">
-
-                      <div style="padding: 15px; width: 550px">
-                        <?php print $body ?>
-                      </div>
-
+                      <div style="padding: 15px"><?php print $body; ?></div>
                     </td>
                   </tr>
                 </table>
@@ -107,19 +114,9 @@
 
                       <table class="colorband">
                         <tr>
-                          <td style='background:"http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png"'
-                            bgcolor="#E4E3E1" width="580" height="6" valign="top">
-                          <!--[if gte mso 9]>
-                          <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:580px;height:6px;">
-                          <v:fill type="tile" src="http://dev.captconnect.edc.org/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" color="#E4E3E1" />
-                          <v:textbox inset="0,0,0,0">
-                          <![endif]-->
+                          <td background="<?php print $base_url; ?>/sites/captconnect.edc.org/themes/capt/images/mail/colorband.png" bgcolor="#E4E3E1" width="580" height="6" valign="top">
                             <div>
                             </div>
-                          <!--[if gte mso 9]>
-                          </v:textbox>
-                          </v:rect>
-                          <![endif]-->
                           </td>
                         </tr>
                       </table>
@@ -132,10 +129,9 @@
 
                       <table class="mailing center">
                         <tr>
-                          <td width="580">
-                            <div style="text-align:left">
-                              <p>Contacts for Questions/Issues: Technical Problems (<a href="mbiewener@edc.org">Matt Biewener</a>), Content and Materials Approval (<a href="madler@edc.org">Melanie Adler</a>)</p>
-                            </div>
+                          <td>
+                            <p>Mailing Address: SAMHSA's Center for the Application of Prevention Technologies<br>Education Development Center, Inc. | 43 Foundry Ave | Waltham, MA 02453-8313</p>
+                            <p><a href="<?php print $base_url; ?>">Terms</a> | <a href="<?php print $base_url; ?>">Privacy</a> | <a href="<?php print $base_url; ?>">Unsubscribe</a></p>
                           </td>
                         </tr>
                       </table>
@@ -145,13 +141,10 @@
                 </table>
 
               </div>
-            <!--[if gte mso 9]>
-              </v:textbox>
-            </v:rect>
-            <![endif]-->
             </td>
           </tr>
         </table>
+
 
 
       </div>
