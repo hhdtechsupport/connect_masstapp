b94c21907b6ccfa792c2a3c6cc4a3d3fde3bca65
Biewener, Matt <mbiewener@edc.org>
2016-03-30 14:01:07 -0400

    Fixed image swaps on event pages for anonymous

--- themes/capt/js/script.js
+++ themes/capt/js/script.js
@@ -622,7 +622,8 @@ function otherSelectbox ($field, $fieldPrev) {
 
   if ($('body').hasClass('logged-in')   ||
       $('body').hasClass('page-events') ||
-      $('body').hasClass('node-type-page')) {
+      $('body').hasClass('node-type-page') ||
+      $('body').hasClass('node-type-event')) {
     $('.header-photos-2 .photos .photo-inner:nth-child(1)').each(function(){ $(this).css('opacity','1'); });
     $('.header-photos-2 .photos .photo-inner:nth-child(2)').each(function(){ $(this).css('opacity','0'); });
     timeout_init(photos, photo_count);
