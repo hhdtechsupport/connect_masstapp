6b78ef545605ef2b808a337290c8517d61d83aa0
Biewener, Matt <mbiewener@edc.org>
2016-04-06 16:23:35 -0400

    Commit mine!

--- themes/capt/js/script.js
+++ themes/capt/js/script.js
@@ -981,7 +981,7 @@ function otherSelectbox ($field, $fieldPrev) {
 
   function getLabelsAndInputs($formItem) {
     var label = $formItem.children('label:not(.option)').clone().children().remove().end().text().trim();
-    else if ($formItem.hasClass('form-type-textfield')){
+    if ($formItem.hasClass('form-type-textfield')){
       var input = $formItem.find('input').val();
       label = label.replace(' (check all that apply):','');
     }
@@ -1011,36 +1011,41 @@ function otherSelectbox ($field, $fieldPrev) {
     var $panel = $(this);
     var label = '';
 
+    $panel.find('.form-wrapper').each(function(){
 
+      $(this).prepend('<div class="content"></div>');
+      $(this).prepend('<div class="editor">Edit</div>');
+      $(this).prepend('<div class="saver">Save</div>');
 
-     $panel.find('.form-wrapper').each(function(){
+      $initialValue = getLabelsAndInputs($(this).find('.form-item'));
+      $(this).find('.content').append($initialValue);
+      $(this).find('.form-item').css('display','none');
 
-       $(this).prepend('<div class="content"></div>');
-       $(this).prepend('<div class="editor">Edit</div>');
-       $(this).prepend('<div class="saver">Save</div>');
+      $(this).children('.editor').on('click',function(){
+        $(this).css('display','none');
+        $(this).parent().children('.saver').css('display','block');
+        $(this).parent().children('.content').css('display','none');
+        var $formItem = $(this).parent().find('.form-item').css('display','block');
+      });
 
-       $(this).children('.editor').on('click',function(){
-         $(this).css('display','none');
-         $(this).parent().children('.saver').css('display','block');
-         $(this).parent().children('.content').css('display','none');
-         var $formItem = $(this).parent().find('.form-item').css('display','block');
-       });
+      $(this).find('.item .value').on('click',function(){
+        $(this).parent().parent().parent().children('.editor').css('display','none');
+        $(this).parent().parent().parent().children('.saver').css('display','block');
+        $(this).parent().parent().parent().children('.content').css('display','none');
+        var $formItem = $(this).parent().parent().parent().find('.form-item').css('display','block');
+      });
 
-       $(this).children('.saver').on('click',function(){
-         $(this).css('display', 'none');
-         $(this).parent().children('.editor').css('display','block');
-         var $formItem = $(this).parent().find('.form-item');
-         $(this).parent().children('.content').html(getLabelsAndInputs($formItem)).css('display','block');
-         $formItem.css('display','none');
-       });
+      $(this).children('.saver').on('click',function(){
+        $(this).css('display', 'none');
+        $(this).parent().children('.editor').css('display','block');
+        var $formItem = $(this).parent().find('.form-item');
+        $(this).parent().children('.content').html(getLabelsAndInputs($formItem)).css('display','block');
+        $formItem.css('display','none');
+      });
 
-       $initialValue = getLabelsAndInputs($(this).find('.form-item'));
-       $(this).find('.content').append($initialValue);
-       $(this).find('.form-item').css('display','none');
+    });
 
-     });
-
-   });
+  });
 
 
 
